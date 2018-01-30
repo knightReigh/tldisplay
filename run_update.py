3089117e4a21ece213ff25332fcc05fae43eb0ed
KnightReigh <12376953+knightReigh@users.noreply.github.com>
2018-01-30 13:16:40 -0600

    Update run_update.py

--- run_update.py
+++ run_update.py
@@ -12,14 +12,20 @@ from config import connection_timeout
 from config import pause_interval
 from config import pause_time
 
-# 更新微博
-w = Weibo(user_id,filter=0)
-w.set_cookie(cookie)
-w.connection_timeout = connection_timeout
-w.pause_interval = pause_interval
-w.pause_time = pause_time
-w.update()
+# 更新微博: 杨冰怡
+for _id in user_id:
+    w = Weibo(_id,filter=0)
+    w.set_cookie(cookie)
+    w.connection_timeout = connection_timeout
+    w.pause_interval = pause_interval
+    w.pause_time = pause_time
+    w.update()
 
-# 创建新的timeline
+# 更新微博: 冯晓菲
+
+# 创建新的timeline 杨冰怡
 from timeline import build_timeline
-build_timeline(user_id, template_name="PinkStar")
+build_timeline(user_id[0], template_name="PinkStar")
+
+# 创建新的timeline 冯晓菲
+build_timeline(user_id[1], template_name="PinkStar_dark")

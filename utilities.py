8cb7e15ef08de68bd1a6c2bdb72b45e72384c55a
KnightReigh <12376953+knightReigh@users.noreply.github.com>
2018-01-30 21:17:23 -0600

    add overwrite to copytree

--- utilities.py
+++ utilities.py
@@ -458,11 +458,12 @@ def check_backup(working_path, frequency='1'):
             for i in range(0, len(files)):
                 shutil.copy2(working_path + os.path.sep + files[i], backup_dir + os.path.sep + files[i])
 
-                
-                
+
+
 def copytree(src, dst, symlinks=False, ignore=None):
-    if not os.path.exists(dst):
-        os.makedirs(dst)
+    if os.path.exists(dst):
+        shutil.rmtree(dst)
+    os.makedirs(dst)
     for item in os.listdir(src):
         s = os.path.join(src, item)
         d = os.path.join(dst, item)
@@ -471,5 +472,5 @@ def copytree(src, dst, symlinks=False, ignore=None):
         else:
             if not os.path.exists(d) or os.stat(s).st_mtime - os.stat(d).st_mtime > 1:
                 shutil.copy2(s, d)
- 
-                
+
+

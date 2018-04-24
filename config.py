c3a5ce1c4e62882c14adb23014ce80754b25cc66
cutelittleturtle <31291893+cutelittleturtle@users.noreply.github.com>
2018-04-24 18:33:13 -0500

    update cookie

--- config.py
+++ config.py
@@ -6,7 +6,7 @@
 user_id = [5491331848, 5461287018] # 杨冰怡、冯晓菲
 weibo_urls = ['https://weibo.com/u/5491331848', 'https://weibo.com/u/5461287018'] # 杨冰怡、冯晓菲
 filter = 0 # 取值范围为0、1，程序默认值为0，代表要爬取用户的全部微博(包括转)，1代表只爬取用户的原创微博
-cookie = {"Cookie":"_T_WM=efd21a0dba0f5a1504c293a195801e63; SUB=_2A253dgV-DeRhGeNH7FEQ9S7KzzuIHXVUmKs2rDV6PUJbkdBeLUHxkW1NSpoOVxM-fws1iAOfmxGpPMYt7AgZJ7nf; SUHB=0FQrfXsPRm2zcb; SCF=Ap3UoqD_tiJ_KAeMT7bH_kON90wGhkBm1BXjO15wpfg-MvOV2-JbHI-MC7ixnntKv5T11l-wnVweej4GVsPYNDs.; SSOLoginState=1517450542"}
+cookie = {"Cookie":"SSOLoginState=1524612702; ALF=1527204668; SCF=AqQguS4NUhkxgfPyNGbbZEz8-Qoo1My2h7vQ3Tj1qjRHLFwOuQ0iOjbpaeSIu1ag5XdoiVT2lH7n9ygpeTeN3xg.; SUHB=0vJY_RCN-WEkoX; _T_WM=6f33c9a8e268ce3ef50138224e2ed203; SUB=_2A253284NDeRhGeBM61sY-SzEwj6IHXVVJ9JFrDV6PUJbktBeLWT1kW1NRQbR-mXogjCOBoJf8qUQMQ88TMDL8aNC; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WFvQATysoA1U3a_JyI.eB_W5JpX5K-hUgL.FoqEeh.41KzR1Kz2dJLoIp7LxKML1KBLBKnLxKqL1hnLBoMp1KM0SK-ceK.0"}
 connection_timeout = 90 # request链接timeout, 秒
 pause_interval = 15 # 读取微博每_页停顿一次，避免短时间内读取过多
 pause_time = 5 # 读取停顿时长,秒
